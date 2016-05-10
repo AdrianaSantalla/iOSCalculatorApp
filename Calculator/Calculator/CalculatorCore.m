d6dd1e11a0b5d1be3eae16ba1225dbd502ef828e
AdrianaSantalla <adriana.santalla@gmail.com>
2016-05-10 10:33:57 -0700

    Validation

--- Calculator/Calculator/CalculatorCore.m
+++ Calculator/Calculator/CalculatorCore.m
@@ -23,20 +23,24 @@
 }
 
 -(double)performOperation:(NSString *)operation {
-    double result = [[_operands objectAtIndex:0] doubleValue];;
-    //double tmp = [operands]
-    for(int i=1; i<[self.operands count]; i++) {
-        if([operation isEqualToString:@"+"])
-            result += [[_operands objectAtIndex:i] doubleValue];
-        if([operation isEqualToString:@"-"])
-            result -= [[_operands objectAtIndex:i] doubleValue];
-        if([operation isEqualToString:@"*"])
-            result *= [[_operands objectAtIndex:i] doubleValue];
-        if([operation isEqualToString:@"/"])
-            result /= [[_operands objectAtIndex:i] doubleValue];
+    double result = 0;
+    //validating if we have at least one operand
+    if(_operands && [_operands count] >= 1) {
+        result = [[self.operands objectAtIndex:0] doubleValue];
+        NSLog(@"tmp result: %f", result);
+        for(int i=1; i<[self.operands count]; i++) {
+            if([operation isEqualToString:@"+"])
+                result += [[_operands objectAtIndex:i] doubleValue];
+            if([operation isEqualToString:@"-"])
+                result -= [[_operands objectAtIndex:i] doubleValue];
+            if([operation isEqualToString:@"*"])
+                result *= [[_operands objectAtIndex:i] doubleValue];
+            if([operation isEqualToString:@"/"])
+                result /= [[_operands objectAtIndex:i] doubleValue];
+        }
+        [_operands removeAllObjects];
+        //[_operands addObject:[NSNumber numberWithDouble:result]];
     }
-    [_operands removeAllObjects];
-    //[_operands addObject:[NSNumber numberWithDouble:result]];
     return result;
 }
 
